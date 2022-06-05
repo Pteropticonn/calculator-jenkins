713fbe58fe02a0961b5419cf6dc3cc1ce8a2488e
Pteropticonn <ParadoxMeltdown@protonmail.com>
2022-06-05 13:49:31 -0400

    Add sum logic, controller and unit tests

--- src/test/java/com/Pteropticon/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/Pteropticon/calculator/CalculatorApplicationTests.java
@@ -2,12 +2,20 @@ package com.Pteropticon.calculator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class CalculatorApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void CalculatorTest() {
+		 final Calculator calculator = new Calculator();
+			assertEquals(5, calculator.sum(2,3));
+
+	}
+
 
 }
